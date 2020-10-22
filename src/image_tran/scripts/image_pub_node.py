ef1e710a8d86ef650b6c1fec14bf73b9624a8753
guiming <guimingthu@163.com>
2020-10-22 03:33:49 -0700

    topic3

--- src/image_tran/scripts/image_pub_node.py
+++ src/image_tran/scripts/image_pub_node.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 #coding:utf-8
-
 import rospy
 import sys
 import cv2
 import os
 import numpy as np
 from sensor_msgs.msg import Image
+import random
 from cv_bridge import CvBridge, CvBridgeError
-
 def pubImage():
-	rospy.init_node('pubImage', anonymous = True)
-	pub = rospy.Publisher('ShowImage', Image, queue_size = 10)
-	rate = rospy.Rate(10)
-	bridge = CvBridge()
-	get_image_name = []
-	path = "/home/nics/catkin_ws/src/image_tran/dataset"
-	for item in os.listdir(path):
-		get_image_name.append(os.path.join(path, item))
-	while not rospy.is_shutdown():
-		for imagepath in get_image_name:
-			image = cv2.imread(imagepath)
-			image = cv2.resize(image, (900, 450))
-			pub.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
-
-			rate.sleep()
-
+    rospy.init_node('pubImage', anonymous = True)
+    pub = rospy.Publisher('ShowImage', Image, queue_size = 10)
+    rate = rospy.Rate(1)
+    bridge = CvBridge()
+    path = "/home/nics/python图像处理+CNN/three_balls.jpg"
+    image = cv2.imread(path)
+    h, w, _ = image.shape
+    while not rospy.is_shutdown():
+        # 生成切分后的图片
+        start_h = int(random.random() * h / 4)
+        height = int(h * 3 / 4)
+        start_w = int(random.random() * w / 4)
+        width = int(w * 3 / 4)
+        transfer_image = image[start_h:(start_h+height),start_w:(start_w+width),:]
+        # 传输切分后的图片
+        pub.publish(bridge.cv2_to_imgmsg(transfer_image, "bgr8"))
+        rate.sleep()
 if __name__ == '__main__':
-	try:
-		pubImage()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        pubImage()
+    except rospy.ROSInterruptException:
+        pass
--- src/image_tran/scripts/image_sub_node.py
+++ src/image_tran/scripts/image_sub_node.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 #coding:utf-8
-
+import os
 import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
+import numpy as np
+import commands
 
 def callback(data):
-	bridge = CvBridge()
-	cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-	cv2.imshow("view", cv_image)
-
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    # save image, detect, read result
+    abs_path = "/home/nics/catkin_ws/src/image_tran/scripts/yolov3_detect"
+    cv2.imwrite("%s/data/samples/three_balls.jpg" % abs_path, cv_image)
+    os.system('cd %s && python detect.py' % abs_path)
+    res_image = cv2.imread("%s/ball_detect.jpg" % abs_path)
+    cv2.imshow("view", res_image)
 def showImage():
-	rospy.init_node('showImage', anonymous = True)
-	cv2.namedWindow("view", cv2.WINDOW_AUTOSIZE)
-	cv2.startWindowThread()
-	rospy.Subscriber('ShowImage', Image, callback)
-	rospy.spin()
-	cv2.destroyWindow("view")
-
+    rospy.init_node('showImage', anonymous = True)
+    cv2.namedWindow("view", cv2.WINDOW_AUTOSIZE)
+    cv2.startWindowThread()
+    rospy.Subscriber('ShowImage', Image, callback)
+    rospy.spin()
+    cv2.destroyWindow("view")
 if __name__ == '__main__':
-	showImage()
+    showImage()
